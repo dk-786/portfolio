85b38e0ec2fe72f5ae7e4c336c648c0dffbe89df
dk-786 <167399983+dk-786@users.noreply.github.com>
2025-09-25 14:34:39 +0530

    solve product grid img size bug

--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -31,7 +31,7 @@ export const ProjectGrid: React.FC<ProjectGridProps> = ({
                 alt={project.title}
                 width={500}
                 height={400}
-                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                className="w-full h-auto aspect-[5/3] object-cover transition-transform duration-500 group-hover:scale-110"
               />
             </div>
             <div className="flex justify-between items-start px-5 py-6 relative">
@@ -48,7 +48,7 @@ export const ProjectGrid: React.FC<ProjectGridProps> = ({
               <button className="hidden lg:flex w-16 h-16 items-center justify-center rounded-md bg-[#111] border border-[#2f3030] text-white hover:bg-[#ffbd4a] hover:text-black transition duration-350"
                onClick={() => {
                 if (typeof window !== "undefined") {
-                  window.scrollTo({ top: 0, behavior: "smooth" });
+                  window.scrollTo({ top: 1550, behavior: "smooth" });
                 }
               }}
               >
